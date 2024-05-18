6810edceb3c38d3c2d5d3104de1a872753c0a11b
Vincent Mukuna <mukunavincent28@gmail.com>
2024-05-18 15:16:34 +0300

    [feat] add database notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,7 @@ class AdminPanelProvider extends PanelProvider
                 'gray' => Color::Gray,
             ])
             ->profile(isSimple: false)
+            ->databaseNotifications()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
